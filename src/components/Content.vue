4680decb7f2bdd1d59a32571686247f129535eaf
Niseeya <15524737300@163.com>
2023-02-23 22:08:33 +0800

    页面构建完成

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,9 +1,10 @@
 <script lang="ts" >
 export default {
-    props: ['id','title','note']
+    props: ['id','title','note','remark']
 }
 </script>
 <template>
    <h2>{{ id }}{{ title }}</h2> 
    <h4>{{ note }}</h4>
+   <h6>{{ remark }}</h6>
 </template>
